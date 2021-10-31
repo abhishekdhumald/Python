0a2fda3f0acc116c19491438d5850653e5cf3afb
Abhishek Dhumal <78952083+abhishek-d-d@users.noreply.github.com>
2021-10-31 09:17:20 +0530

    Update areaOfcircle.py

--- Basics/areaOfcircle.py
+++ Basics/areaOfcircle.py
@@ -1,5 +1,10 @@
 #   Write a Python program which accepts the radius of a circle from the user and compute the area.
+
+from math import pi
+
 print("Enter the radius of the circle")
 radius = float(input("radius = : "))
 area = 3.14*(radius**2)
+Area = pi*radius**2
 print("Area of circle with {0} radius is {1}".format(radius,area))
+print("Area of circle with {0} radius is {1}".format(radius,Area))
