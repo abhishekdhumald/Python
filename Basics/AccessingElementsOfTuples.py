415252c3fca05fc99ce4c79c2f416edaa8f07bcb
Abhishek Dhumal <78952083+abhishek-d-d@users.noreply.github.com>
2021-10-31 09:58:03 +0530

    Accessing Elements Of Tuples
    #   Write a Python program to display the examination schedule. (extract the date from exam_st_date).

---
+++ Basics/AccessingElementsOfTuples.py
@@ -0,0 +1,11 @@
+#   Write a Python program to display the examination schedule. (extract the date from exam_st_date).
+
+
+exam_st_date = (11,12,2014)
+print( "The examination will start from : %s / %s / %s"%exam_st_date)
+
+#   For input 
+
+# exam_st_date = input("Enter exam dates separated by comas :")
+# abc = tuple(exam_st_date.split(","))
+# print( "The examination will start from : %s / %s / %s"%abc)
