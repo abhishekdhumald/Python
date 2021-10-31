7f1efe955b41e2f523b3d92169879350cff60bbe
Abhishek Dhumal <78952083+abhishek-d-d@users.noreply.github.com>
2021-10-31 09:46:07 +0530

    Accessing List elements using indexing
    a Python program to display the first and last colors from the following list.

---
+++ Basics/accessingList.py
@@ -0,0 +1,7 @@
+#   Write a Python program to display the first and last colors from the following list.
+
+color_list = ["Red","Green","White" ,"Black"]
+
+print("First colour is " +str(color_list[0])+" and Last colour is "+str(color_list[-1])+" from list"+str(color_list))
+
+print("First colour is {0} and Last colour is {1} from list {2}".format(str(color_list[0]),str(color_list[-1]),str(color_list)))
